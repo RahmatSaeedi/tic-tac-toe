ed54e056f5c8798d96293aa9da4b2c3ef2e78f2c
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-10-07 11:21:17 -0600

    ensuring when game is wone no more click is accepted

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import Board from './Board';
+import React, { Component } from 'react'
+import Board from './Board'
 
 export class Game extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state ={
       xIsNext: true,
       stepNumber: 0,
@@ -13,12 +13,16 @@ export class Game extends Component {
         }
       ]
     }
-    //this.onClick = this.onClick.bind(this)
   }
   handleClick(i){
     const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1]
     const squares = current.squares.slice()
+    const winner = calculateWinner(squares)
+
+    if(winner || squares[i]){
+      return
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       history: history.concat({
@@ -27,6 +31,7 @@ export class Game extends Component {
       xIsNext: !this.state.xIsNext,
       stepNumber: history.length
     })
+    
   }
 
   render() {
@@ -45,10 +50,29 @@ export class Game extends Component {
 }
 
 
+function calculateWinner(board) {
+  const winningStrategies = [
+    //Horizontal ways of winning
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    
+    //vertical ways of winning
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
 
-
-
-  return null;
+    //Diagonal ways of winning
+    [0,4,8],
+    [2,4,6]
+  ]
+  for(let i = 0; i < winningStrategies.length; i++){
+    const [a,b,c] = winningStrategies[i];
+    if (board[a] && board[a] === board[b] && board[b] === board[c]){
+      return board[a]
+    }
+  }
+  return null
 }
 
-export default Game;
+export default Game
