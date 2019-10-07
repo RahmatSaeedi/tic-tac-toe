1727afc3ed3469d629ee383d8f676d41b2b632a2
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-10-07 10:36:58 -0600

    overall game design

---
+++ src/components/Game.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Board from './Board';
+
+export class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      xIsNext: true,
+      stepNumber: 0,
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ]
+    }
+    //this.onClick = this.onClick.bind(this)
+  }
+  handleClick(i){
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
+    const current = history[history.length - 1]
+    const squares = current.squares.slice()
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      history: history.concat({
+        squares
+      }),
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length
+    })
+  }
+
+  render() {
+    const history = this.state.history
+    const current = history[this.state.stepNumber]
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board onClick={(i) => this.handleClick(i)}
+            squares={current.squares}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
+
+
+  return null;
+}
+
+export default Game;
